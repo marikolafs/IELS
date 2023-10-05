6233d117233fd869c94b6b279862ddab3a1045b2
marikolafs <airamolafs@gmail.com>
2023-10-05 16:00:03 +0200

    attempt at sensor input

--- servo/servo.ino
+++ servo/servo.ino
@@ -2,33 +2,66 @@
 
 const int servo1 = 9;
 const int servo2 = 10;
-int potensiometer1 = A0;
-int potensiometer2 = A1;
 
-int pos = 0;
-int val1;
-int val2;
+int rotasjonsVerdi = 90;
+int rotasjonsverdiUnder = 90; 
+
+int sensors[] = {sensor1, sensor2, sensor3, sensor4};
+int sensorCount = 4; 
+int valuelist[] = {0, 0, 0, 0};
+int sensorMax[] = {100, 100, 100, 100};
 
 Servo firstServo;
 Servo secondServo;
 
 void setup(){
 
-firstServo.attach(servo1);
-secondServo.attach(servo2);
+    firstServo.attach(servo1);
+    secondServo.attach(servo2);
 
+
+    Serial.begin(9600);
+
+    for(int i = 0; i < sensorCount; i ++){
+        pinMode(sensors[i], INPUT);
+    }
+
+    while(millis() < 5000){
+        for(int i = 0; i < 4; i++){
+            sensorValue = analogRead(sensors[i]);
+
+             if(sensorValue > sensorMax[i]){
+
+                sensorMax[i] = sensorValue; 
+         }
+
+        }
+    }
 }
+
+
 
 void loop(){
 
-val1 = analogRead(potensiometer1);
-val1 = map(val1, 0, 1023, 0, 180);
-firstServo.write(val1);
-delay(15);
+   for(int i = 0; i < 4; i ++){
+    valuelist[i] = analogRead(sensors[i]);
+    valuelist[i] = map(valuelist[i], 0, sensorMax[i], 0, 100);
+   }
 
-val2 = analogRead(potensiometer2);
-val2 = map(val2, 0, 1023, 0, 180);
-secondServo.write(val2);
-delay(15);
+   if((valuelist[0] + valuelist[3]) - (valuelist[1] + valuelist[2]) > 7){
+
+    rotasjonsVerdi -= 2;
+    rotasjonsverdiUnder +=2;
+
+   } else if((valuelist[1] + valuelist[2]) - (valuelist[0] + valuelist[3]) > 7){
+
+    rotasjonsVerdi +=2;
+    rotasjonsverdiUnder -=2;
+
+   }
+
+   firstServo.write(rotasjonsVerdi);
+   secondServo.write(rotasjonsverdiUnder);
+
 
 }
